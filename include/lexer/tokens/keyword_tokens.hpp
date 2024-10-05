ae5c8a66bbc6cf8815ec443374d12ed97e43e437
marcelnoehre <marcelnoehre@web.de>
2024-10-05 22:38:14 +0200

    remove copied lexer files

--- include/lexer/tokens/keyword_tokens.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef KEYWORD_TOKEN_HPP
-#define KEYWORD_TOKEN_HPP
-
-enum class KeywordToken { VARIABLE, CONSTANT, IF, ELSE, ELSE_IF, FOR, WHILE, DO, FUNCTION, RETURN, IN, OF };
-
-#endif
--- include/lexer/tokens/log_tokens.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef LOG_TOKEN_HPP
-#define LOG_TOKEN_HPP
-
-enum class LogToken { BASIC, COLORED, WARN, ERROR };
-
-#endif
