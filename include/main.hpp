5230c709f3488499636f7db13178cb5dad1cc484
marcelnoehre <marcelnoehre@web.de>
2024-10-19 11:52:54 +0200

    adjust main script

--- include/main.hpp
+++ include/main.hpp
@@ -1,4 +1,12 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "lexer/lexer.hpp"
+#include "parser/parser.hpp"
+
 #endif
