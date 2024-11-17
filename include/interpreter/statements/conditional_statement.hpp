cc9f8cfed6b9aa77765ee47158518484b464c897
marcelnoehre <marcelnoehre@web.de>
2024-11-17 15:18:36 +0100

    apply clang format

--- include/interpreter/statements/conditional_statement.hpp
+++ include/interpreter/statements/conditional_statement.hpp
@@ -1,8 +1,8 @@
 #ifndef CONDITIONAL_STATEMENT_HPP
 #define CONDITIONAL_STATEMENT_HPP
 
-#include "interpreter/statements/statement.hpp"
 #include "interpreter/expressions/expression.hpp"
+#include "interpreter/statements/statement.hpp"
 #include "interpreter/storage.hpp"
 #include "parser/ast_nodes.hpp"
 
