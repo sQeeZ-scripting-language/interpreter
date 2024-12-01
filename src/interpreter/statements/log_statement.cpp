02bc2646ccf8267ccb4f1a60a54ac23e26894a60
marcelnoehre <marcelnoehre@web.de>
2024-12-01 15:16:19 +0100

    log arrays

--- src/interpreter/statements/log_statement.cpp
+++ src/interpreter/statements/log_statement.cpp
@@ -8,10 +8,17 @@ void LogStatement::execute() {
   if (logNode->logType.tag != Token::TypeTag::LOG) {
     throw std::logic_error("Expected log token.");
   }
+  if (!logNode->message) {
+    return;
+  }
+
+  std::string printableValue = getPrintableValue(
+      Expression(dynamic_cast<Expr *>(logNode->message.get()), storage)
+          .execute());
 
   switch (logNode->logType.type.logToken) {
   case LogToken::BASIC:
-    std::cout << getPrintableValue() << std::endl;
+    std::cout << printableValue << std::endl;
     break;
 
   case LogToken::COLORED:
@@ -19,18 +26,18 @@ void LogStatement::execute() {
     if (auto *hexCode = dynamic_cast<HexCodeLiteral *>(logNode->color.get())) {
       hexToRGB(hexCode->value, r, g, b);
       std::cout << "\033[38;2;" << r << ";" << g << ";" << b << "m"
-                << getPrintableValue() << "\033[0m" << std::endl;
+                << printableValue << "\033[0m" << std::endl;
     } else {
       throw std::invalid_argument("Expected hex code for colored log message.");
     }
     break;
 
   case LogToken::WARN:
-    std::cout << "\033[33m" << getPrintableValue() << "\033[0m" << std::endl;
+    std::cout << "\033[33m" << printableValue << "\033[0m" << std::endl;
     break;
 
   case LogToken::ERROR:
-    std::cout << "\033[31m" << getPrintableValue() << "\033[0m" << std::endl;
+    std::cout << "\033[31m" << printableValue << "\033[0m" << std::endl;
     break;
 
   default:
@@ -38,30 +45,31 @@ void LogStatement::execute() {
   }
 }
 
-std::string LogStatement::getPrintableValue() {
-  if (logNode->message) {
-    Storage::DataWrapper value =
-        Expression(dynamic_cast<Expr *>(logNode->message.get()), storage)
-            .execute();
-    switch (value.dataType) {
-    case Storage::DataType::INTEGER:
-      return std::to_string(value.data._int);
-    case Storage::DataType::DOUBLE:
-      return std::to_string(value.data._double);
-    case Storage::DataType::BOOLEAN:
-      return value.data._bool ? "true" : "false";
-    case Storage::DataType::CHAR:
-      return std::string(1, value.data._char);
-    case Storage::DataType::STRING:
-    case Storage::DataType::HEXCODE:
-      return *(value.data._string);
-    case Storage::DataType::_NULL:
-      return "null";
-    default:
-      throw std::invalid_argument("Unknown data type!");
+std::string LogStatement::getPrintableValue(Storage::DataWrapper value) {
+  std::string result = "";
+  switch (value.dataType) {
+  case Storage::DataType::INTEGER:
+    return std::to_string(value.data._int);
+  case Storage::DataType::DOUBLE:
+    return std::to_string(value.data._double);
+  case Storage::DataType::BOOLEAN:
+    return value.data._bool ? "true" : "false";
+  case Storage::DataType::CHAR:
+    return std::string(1, value.data._char);
+  case Storage::DataType::STRING:
+  case Storage::DataType::HEXCODE:
+    return *(value.data._string);
+  case Storage::DataType::ARRAY:
+    result = "[";
+    for (const auto &element : *(value.data._array)) {
+      result += getPrintableValue(element) +
+                (&element != &value.data._array->back() ? ", " : "");
     }
-  } else {
-    throw std::invalid_argument("No log message provided.");
+    return result + "]";
+  case Storage::DataType::_NULL:
+    return "null";
+  default:
+    throw std::invalid_argument("Unknown data type!");
   }
 }
 
