f78e004d8bb062bb54e39a9881956d4e0ee13468
marcelnoehre <marcelnoehre@web.de>
2025-04-06 09:42:38 +0200

    apply clang format

--- src/interpreter/node/interpreter_node.cpp
+++ src/interpreter/node/interpreter_node.cpp
@@ -1,25 +1,32 @@
 #include "interpreter/node/interpreter_node.hpp"
 
-InterpreterNode::InterpreterNode(const Napi::CallbackInfo &args) : ObjectWrap(args) {
+InterpreterNode::InterpreterNode(const Napi::CallbackInfo &args)
+    : ObjectWrap(args) {
   this->_env = args.Env();
   if (args.Length() < 1 || !args[0].IsFunction()) {
-    Napi::TypeError::New(this->_env, "Function expected").ThrowAsJavaScriptException();
+    Napi::TypeError::New(this->_env, "Function expected")
+        .ThrowAsJavaScriptException();
   }
   Napi::Function require = args[0].As<Napi::Function>();
-  std::regex self_regex("^function require\\(path\\)", std::regex_constants::ECMAScript | std::regex_constants::icase);
+  std::regex self_regex("^function require\\(path\\)",
+                        std::regex_constants::ECMAScript |
+                            std::regex_constants::icase);
   if (!std::regex_search(require.ToString().Utf8Value().c_str(), self_regex)) {
-    Napi::TypeError::New(this->_env, "{require} Function expected").ThrowAsJavaScriptException();
+    Napi::TypeError::New(this->_env, "{require} Function expected")
+        .ThrowAsJavaScriptException();
   }
   this->_require = Persistent(require);
 }
 
 Napi::String InterpreterNode::pingInstance(const Napi::CallbackInfo &args) {
-  return Napi::String::New(this->_env, "Node instance of the sQeeZ-Interpreter is working!");
+  return Napi::String::New(
+      this->_env, "Node instance of the sQeeZ-Interpreter is working!");
 }
 
 void InterpreterNode::interpret(const Napi::CallbackInfo &args) {
   if (args.Length() < 1 || !args[0].IsString()) {
-    Napi::TypeError::New(this->_env, "Code expected!").ThrowAsJavaScriptException();
+    Napi::TypeError::New(this->_env, "Code expected!")
+        .ThrowAsJavaScriptException();
   }
   Napi::Env env = args.Env();
   auto code = args[0].As<Napi::String>().Utf8Value();
@@ -32,7 +39,8 @@ void InterpreterNode::interpret(const Napi::CallbackInfo &args) {
   Parser parser(tokens);
   std::unique_ptr<Program> program = parser.parse(devMode);
   if (program == nullptr) {
-    Napi::TypeError::New(env, "Failed to parse the code!").ThrowAsJavaScriptException();
+    Napi::TypeError::New(env, "Failed to parse the code!")
+        .ThrowAsJavaScriptException();
   }
   Interpreter interpreter(std::move(program));
   interpreter.interpret(devMode);
@@ -41,8 +49,12 @@ void InterpreterNode::interpret(const Napi::CallbackInfo &args) {
 Napi::Function InterpreterNode::GetClass(Napi::Env env) {
   return DefineClass(
       env, "InterpreterNode",
-      {InterpreterNode::InstanceMethod("pingInstance", reinterpret_cast<InstanceMethodCallback>(&InterpreterNode::pingInstance)),
-       InterpreterNode::InstanceMethod("interpret", reinterpret_cast<InstanceMethodCallback>(&InterpreterNode::interpret))});
+      {InterpreterNode::InstanceMethod("pingInstance",
+                                       reinterpret_cast<InstanceMethodCallback>(
+                                           &InterpreterNode::pingInstance)),
+       InterpreterNode::InstanceMethod("interpret",
+                                       reinterpret_cast<InstanceMethodCallback>(
+                                           &InterpreterNode::interpret))});
 }
 
 Napi::String pingInterpreter(const Napi::CallbackInfo &args) {
@@ -67,7 +79,8 @@ Napi::String info(const Napi::CallbackInfo &args) {
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set(Napi::String::New(env, "pingInterpreter"), Napi::Function::New(env, pingInterpreter));
+  exports.Set(Napi::String::New(env, "pingInterpreter"),
+              Napi::Function::New(env, pingInterpreter));
   exports.Set(Napi::String::New(env, "info"), Napi::Function::New(env, info));
   Napi::String name = Napi::String::New(env, "InterpreterNode");
   exports.Set(name, InterpreterNode::GetClass(env));
